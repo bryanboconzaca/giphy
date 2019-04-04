609b79690079793b94094417c4ac2afc09efb25a
bryan boconzaca <bboconzaca2022@bklawtech.com>
2019-04-04 22:31:38 +0000

    put a gif in the page

--- js/script.js
+++ js/script.js
@@ -2,9 +2,20 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 /* global $ */
-
 $("#search-button").click(function(){
-  
+  var searchterm= $("#search-term").val()
+  var custom_api_url="https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC"
+  .ajax({
+        url: custom_api_url,
+        method: "GET",
+        success: function(response) {
+          console.log(response);
+           $("body").append("<img src="+response.data[0].images.original.url+" />");
+            // YOUR CODE GOES HERE
+        
+        }
+    }); 
+    
   
   
 });
